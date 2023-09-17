d58f59fc9246fa9c2bdd30219b8783c40e09f67b
Thiago Tolotti <thiagotolotti@gmail.com>
2023-09-17 20:55:29 -0300

    initial

---
+++ sorting.js
@@ -0,0 +1,87 @@
+const livros = [25,15,30,50,45,20]
+
+//Destrói o original
+let sortByLowestVal = (arr)=>{
+    let result = []
+
+    while(arr.length > 0){
+        let index = 0, lowestVal = 0
+
+        while(index < arr.length){
+            if(arr[index] < arr[lowestVal]) lowestVal = index
+    
+            index++
+        }
+    
+        result.push(arr[lowestVal])
+        arr.splice(lowestVal, 1)
+    }
+
+    return result
+}
+
+// tempo = O(n²)
+//Modifica o original/Mantém o original (controle no início do algoritmo)
+sortByLowestVal = (arrOriginal)=>{
+    let arr = arrOriginal
+    arr = [...arrOriginal] //Cria uma cópia do original (mantém original intacto)
+
+    for(let count = 0; count < arr.length; count++){
+        let index = count, lowestVal = count
+
+        while(index < arr.length){
+            if(arr[index] > arr[lowestVal]) lowestVal = index
+    
+            index++
+        }
+
+        const val = arr[lowestVal]
+        arr.splice(lowestVal, 1)
+        arr.splice(0,0,val)
+    }
+
+    return arr
+}
+
+// Destrói o original
+let recursiveSortByLowestVal = (arr, result = [])=>{
+    if(arr.length <= 0) return result
+
+    let index = 0, lowestVal = 0 
+    
+    while(index < arr.length){
+        if(arr[index] < arr[lowestVal]) lowestVal = index
+
+        index++
+    }    
+
+    result.push(arr[lowestVal])
+    arr.splice(lowestVal, 1)
+
+    return recursiveSortByLowestVal(arr, result)
+}
+
+//Modifica o original/Mantém o original (controle no início do algoritmo)
+recursiveSortByLowestVal = (arrOriginal, count = 0)=>{
+    let arr = arrOriginal
+    arr = [...arrOriginal] //Cria uma cópia do original (mantém original intacto)
+    
+    if(count >= arr.length) return arr
+
+    let lowestVal = count
+    
+    for(let index = count; index < arr.length; index++){
+        if(arr[index] > arr[lowestVal]) lowestVal = index
+    }
+
+    const val = arr[lowestVal]
+    arr.splice(lowestVal, 1)
+    arr.splice(0,0,val)
+
+    return recursiveSortByLowestVal(arr, count + 1)
+}
+
+console.clear()
+// console.log(sortByLowestVal(livros))
+console.log(recursiveSortByLowestVal(livros))
+console.log(livros)
