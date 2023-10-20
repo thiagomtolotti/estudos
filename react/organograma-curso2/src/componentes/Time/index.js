f22628f9da591d3e5945551b1b511ca14cbd4cb2
Thiago Tolotti <thiago.tolotti@riole.com>
2023-10-20 13:30:58 -0300

    .

---
+++ react/organograma-curso2/src/componentes/Time/index.js
@@ -0,0 +1,24 @@
+import Colaborador from '../Colaborador'
+import hexToRgba from 'hex-to-rgba'
+import './time.css'
+
+const Time = ({ time, colaboradores, aoDeletar, mudarCor }) => {
+    return (
+
+        colaboradores.length > 0 && <section className='time' style={{ backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: hexToRgba(time.cor, '0.2')}}>
+
+            <input type='color' className='input-cor' value={time.cor} onChange={(ev)=>mudarCor(ev.target.value, time.id)}/>
+
+            <h3 style={{ borderColor: time.cor }}>{time.nome}</h3>
+
+            <div className='colaboradores'>
+                {colaboradores.map((colaborador, indice) => {
+                    return <Colaborador key={indice} colaborador={colaborador} corDeFundo={time.cor} aoDeletar={aoDeletar}/>
+                })}
+            </div>
+        </section>
+
+    )
+}
+
+export default Time
