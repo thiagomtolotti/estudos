408f5009c0a255195d5dff64d3e35b44b911772b
Thiago Miquelleto Tolotti <thiagotolotti@gmail.com>
2023-12-03 15:59:07 -0300

    Aula 3

--- typescript/curso1/app/app.ts
+++ typescript/curso1/app/app.ts
@@ -1,5 +1,9 @@
-import { Negociacao } from "./models/negociacao.js";
+import { NegociacaoController } from "./controllers/negociacao-controller.js";
 
-const negociacao = new Negociacao(new Date(), 10, 100);
+const controller = new NegociacaoController();
+const form = document.querySelector(".form");
 
-console.log(negociacao);
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  controller.adiciona();
+});
