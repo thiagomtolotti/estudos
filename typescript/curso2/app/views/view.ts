c745b576b946dbfebd6fe7c3dea94e607c9ff78a
Thiago Miquelleto Tolotti <thiagotolotti@gmail.com>
2023-12-18 13:02:39 -0300

    Aula 4

--- typescript/curso2/app/views/view.ts
+++ typescript/curso2/app/views/view.ts
@@ -1,14 +1,24 @@
 export abstract class View<T> {
   protected elemento: HTMLElement;
+  private escapar = false;
 
-  constructor(seletor: string) {
+  constructor(seletor: string, escapar?: boolean) {
     this.elemento = document.querySelector(seletor);
+
+    if (escapar) {
+      this.escapar = escapar;
+    }
   }
 
   protected abstract template(model: T): string;
 
   public update(model: T) {
-    const template = this.template(model);
+    let template = this.template(model);
+
+    if (this.escapar) {
+      template = template.replace(/<script>[\s\S]*?<script>/, "");
+    }
+
     this.elemento.innerHTML = template;
   }
 }
