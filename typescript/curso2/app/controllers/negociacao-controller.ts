b0b498abe0b8f083d6467ed5c27f7e512409a8e6
Thiago Miquelleto Tolotti <thiagotolotti@gmail.com>
2023-12-18 12:52:47 -0300

    Aula 4.3

--- typescript/curso2/app/controllers/negociacao-controller.ts
+++ typescript/curso2/app/controllers/negociacao-controller.ts
@@ -20,7 +20,11 @@ export class NegociacaoController {
   }
 
   public adiciona(): void {
-    const negociacao = this.criaNegociacao();
+    const negociacao = Negociacao.criaDe(
+      this.inputData.value,
+      this.inputQuantidade.value,
+      this.inputValor.value
+    );
 
     if (!this.ehDiaUtil(negociacao.data)) {
       this.mensagemView.update("Apenas negociações em dias úteis são aceitas");
@@ -30,14 +34,6 @@ export class NegociacaoController {
     this.negociacoes.adiciona(negociacao);
     this.atualizaView();
     this.limparFormulario();
-  }
-
-  private criaNegociacao(): Negociacao {
-    const exp = /-/g;
-    const date = new Date(this.inputData.value.replace(exp, ","));
-    const quantidade = parseInt(this.inputQuantidade.value);
-    const valor = parseFloat(this.inputValor.value);
-    return new Negociacao(date, quantidade, valor);
   }
 
   private limparFormulario(): void {
