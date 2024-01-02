f51a650ee92ea44f86adfa94f42ea0fa1ea0d10e
Thiago Miquelleto Tolotti <thiagotolotti@gmail.com>
2024-01-02 17:07:27 -0300

    Aula 1

---
+++ typescript/curso3/app/src/decorators/logar-tempo-de-execucao.ts
@@ -0,0 +1,25 @@
+export function logarTempoDeExecucao() {
+  return function (
+    target: any,
+    propertyKey: string,
+    descriptor: PropertyDescriptor
+  ) {
+    const metodoOriginal = descriptor.value;
+
+    descriptor.value = function (...args: Array<any>) {
+      const t1 = performance.now();
+
+      //Chama o método original
+      const retorno = metodoOriginal.apply(this, args);
+
+      const t2 = performance.now();
+      console.log(
+        `${propertyKey}, tempo de execução: ${(t2 - t1) / 1000} segundos`
+      );
+
+      return retorno;
+    };
+
+    return descriptor;
+  };
+}
