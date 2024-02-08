7d6017df5f8f85dd1672d2fea13bf15d03a0bdff
Thiago Tolotti <thiagotolotti@gmail.com>
2024-02-08 13:47:50 -0300

    chapter 9

--- next/nextjs-dashboard/app/lib/data.ts
+++ next/nextjs-dashboard/app/lib/data.ts
@@ -39,6 +39,9 @@ export async function fetchLatestInvoices() {
   noStore();
 
   try {
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
@@ -50,6 +53,8 @@ export async function fetchLatestInvoices() {
       ...invoice,
       amount: formatCurrency(invoice.amount),
     }));
+
+    console.log('Data fetch completed after 5 seconds.');
     return latestInvoices;
   } catch (error) {
     console.error('Database Error:', error);
