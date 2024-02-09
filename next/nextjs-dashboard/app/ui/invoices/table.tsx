e2798a3e0573f8b622306f9dd2142045253df1df
Thiago Tolotti <thiagotolotti@gmail.com>
2024-02-09 17:26:30 -0300

    chapter 12

--- next/nextjs-dashboard/app/ui/invoices/table.tsx
+++ next/nextjs-dashboard/app/ui/invoices/table.tsx
@@ -13,8 +13,6 @@ export default async function InvoicesTable({
 }) {
   const invoices = await fetchFilteredInvoices(query, currentPage);
 
-  console.log('Load Invoices');
-
   return (
     <div className="mt-6 flow-root">
       <div className="inline-block min-w-full align-middle">
