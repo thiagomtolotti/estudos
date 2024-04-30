b33b2cc0914acd07184d4c4fc4242ba99f2e6a6c
Thiago Tolotti <thiagotolotti@gmail.com>
2024-04-30 12:10:43 -0300

    Chapter 15

--- next/nextjs-dashboard/app/lib/actions.ts
+++ next/nextjs-dashboard/app/lib/actions.ts
@@ -1,9 +1,11 @@
 'use server';
 
 import { sql } from '@vercel/postgres';
+import { signIn } from '@/auth';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import { z } from 'zod';
+import { AuthError } from 'next-auth';
 
 const FormSchema = z.object({
   id: z.string(),
@@ -104,5 +106,25 @@ export async function deleteInvoice(id: string) {
     return {
       message: 'Database Error. Failed to Delete Invoice.',
     };
+  }
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong';
+      }
+    }
+
+    throw error;
   }
 }
