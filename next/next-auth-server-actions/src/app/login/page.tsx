9e78c317dded165d782c548eb2c95333e4e627d3
Thiago Miquelleto Tolotti <thiagotolotti@gmail.com>
2024-05-11 12:13:40 -0300

    'setup'

---
+++ next/next-auth-server-actions/src/app/login/page.tsx
@@ -0,0 +1,12 @@
+export default function Login() {
+  return (
+    <div>
+      <h1>Login</h1>{" "}
+      <form action="">
+        <input type="email" name="email" />
+        <input type="password" name="password" />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
---
+++ next/next-auth-server-actions/src/app/signup/page.tsx
@@ -0,0 +1,12 @@
+export default function Signup() {
+  return (
+    <div>
+      <h1>SignUp</h1>{" "}
+      <form action="">
+        <input type="email" name="email" />
+        <input type="password" name="password" />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
