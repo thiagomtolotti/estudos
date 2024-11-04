8aae5e41c93e4ad24c1f509ef2fb10d64ecd8377
Thiago Tolotti <thiagotolotti@gmail.com>
2024-11-04 19:39:21 -0300

    adds bubble sort

--- ordenacao/cs50/sorting.c
+++ ordenacao/cs50/sorting.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+
 
 #define BUFFER_SIZE 6
 #define LIST_SIZE 5000
@@ -10,6 +12,8 @@ char numberString[BUFFER_SIZE];
 int numbers[LIST_SIZE];
 
 void selection_sort(int numbers[], int length);
+void copy_array(int copy_arr[], int original_arr[], int length);
+void bubble_sort(int numbers[], int length);
 
 int main(void) {
 	char filename[] ="./files/random5000.txt";
@@ -25,10 +29,17 @@ int main(void) {
 		i++;
 	}
 
-	selection_sort(numbers, LIST_SIZE);
+	int selection_sort_numbers[LIST_SIZE];
+	int bubble_sort_numbers[LIST_SIZE];
+	copy_array(selection_sort_numbers, numbers, LIST_SIZE);
+	copy_array(bubble_sort_numbers, numbers, LIST_SIZE);
+
+	selection_sort(selection_sort_numbers, LIST_SIZE);
+	bubble_sort(bubble_sort_numbers, LIST_SIZE);
+	
 	
 	// for (int i = 0; i < LIST_SIZE; i++){
-	// 	printf("%i\n", numbers[i]);
+	// 	printf("%i\n", bubble_sort_numbers[i]);
 	// }
 	
 }
@@ -54,5 +65,38 @@ void selection_sort(int numbers[], int length)
 		numbers[j] = smallest;
 	}
 
-	printf("ORDERED IN %i STEPS", steps);
+	printf("SELECTION SORT: ORDERED IN %i STEPS\n", steps);
+}
+
+void bubble_sort(int numbers[], int length){
+	int steps = 0;
+
+	for(int i = 0; i < length; i++)
+	{
+		bool isSorted = true;
+
+		for(int j = 0; j < length - i - 1; j++)
+		{
+			if(numbers[j] > numbers[j + 1]){
+				int temp = numbers[j];
+				
+				numbers[j] = numbers[j + 1];
+				numbers[j + 1] = temp;
+
+				isSorted = false;
+			}
+
+			steps++;
+		}
+
+		if(isSorted) break;
+	}
+	
+	printf("BUBBLE SORT   : ORDERED IN %i STEPS\n", steps);
+}
+
+void copy_array(int copy_arr[], int original_arr[], int length){
+	for(int i = 0; i < length; i++){
+		copy_arr[i] = original_arr[i];
+	}
 }
